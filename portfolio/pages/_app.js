da6200a8b24a24c1df52f403262cc970ce3bad58
RawandDev <rawand.200091384037@gmail.com>
2021-11-25 15:11:07 +0300

    setup(routing): add routing to navbar

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -1,7 +1,17 @@
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="bg-primary">
+      <Head>
+        <title>Rawand's Portfolio</title>
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
