39f46e1134e8d8c3904900916f2f05f2c258269e
RawandDev <rawand.200091384037@gmail.com>
2021-11-25 15:10:42 +0300

    setup(navbar): add navbar

---
+++ portfolio/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../public/logo.svg";
+
+function Navbar() {
+  return (
+    <div className="text-white md:flex md:justify-around md:items-center">
+      <div>
+        <Image src={Logo} />
+      </div>
+      <div>
+        <ul className="md:flex md:justify-between w-96 text-xl">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/work">
+            <a>Work</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
