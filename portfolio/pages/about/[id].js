591492b3192b845803878fd241f9fb2bf61898a6
RawandDev <rawand.200091384037@gmail.com>
2021-11-28 00:55:06 +0300

    feat(achievement-details): refactor about section with adding details for achievement

---
+++ portfolio/pages/about/[id].js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useRouter } from "next/router";
+import data from "../../components/data.json";
+import Image from "next/image";
+
+function Details() {
+  const router = useRouter();
+
+  const { id } = router.query;
+
+  console.log(router);
+
+  const displayData = data.find((item) => item.id === id);
+
+  console.log(displayData);
+
+  const src1 = { src: displayData?.imageURL1 };
+  const src2 = { src: displayData?.imageURL2 };
+
+  // console.log(src1.src);
+
+  return (
+    <div className="text-white flex flex-col justify-center items-center mt-10">
+      <h1 className="font-bold text-2xl">{displayData?.title}</h1>
+      <span className="text-sm mb-4">{displayData?.date}</span>
+      <p className="max-w-4xl font-medium text-lg">{displayData?.content}</p>
+      {displayData?.imageURL1 && displayData?.imageURL2 && (
+        <div>
+          <Image
+            {...src1}
+            width={600}
+            height={400}
+            objectFit="contain"
+            alt="image"
+          />
+          <Image
+            {...src2}
+            width={600}
+            height={400}
+            objectFit="contain"
+            alt="image"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Details;
