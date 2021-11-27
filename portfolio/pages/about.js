f544c98d11531147ea38766e2b009ee041126e4a
RawandDev <rawand.200091384037@gmail.com>
2021-11-27 19:07:02 +0300

    feat(about): add achievements

--- portfolio/pages/about.js
+++ portfolio/pages/about.js
@@ -4,6 +4,7 @@ import team from "../public/images/team.svg";
 import freelancer from "../public/images/freelancer.svg";
 import brush from "../public/images/brush.svg";
 import company from "../public/images/company.svg";
+import rocket from "../public/images/rocket.png";
 import GeneralSkills from "../components/GeneralSkills";
 
 function About() {
@@ -13,14 +14,24 @@ function About() {
         <h1 className="text-7xl sm:text-10xl font-extrabold opacity-10">
           .About
         </h1>
-        <p className="text-xl font-semibold sm:text-lg">
-          I’m a self-taught programmer and an undergraduate student currently
-          studying Software Engineering at Koya University.
-        </p>
-        <p className="text-xl font-semibold">
-          I'm always looking for new challenges and opportunities to learn new
-          things and improve my skills.
-        </p>
+        <div className="text-xl font-semibold md:max-w-xl">
+          <p>
+            I’m a self-taught programmer and an undergraduate student currently
+            studying Software Engineering at Koya University.
+          </p>
+          <p>
+            I'm always looking for new challenges and opportunities to learn new
+            things and improve my skills.
+          </p>
+        </div>
+        <div className="absolute right-0 top-0 md:top-32">
+          <Image
+            src={rocket}
+            width={300}
+            height={300}
+            className="opacity-10 animate-pulse"
+          />
+        </div>
         {/* <div>
         <h2>Techincal Skills</h2>
         <ul className="text-lg">
@@ -69,6 +80,51 @@ function About() {
             title="Freelancing 💻"
             content="Freelancing/remote is my priority.Let's build your website."
           />
+        </ul>
+      </div>
+      <div>
+        <h2 className="text-center text-white font-extrabold text-2xl mt-10">
+          Achievements
+        </h2>
+        <ul className="relative text-white ml-10 text-lg flex flex-col justify-center items-center">
+          <li className="max-w-md relative pb-10 mb-10 cursor-pointer shadow-xl rounded-lg hover:bg-darkPrimary transition-all duration-300">
+            <svg
+              width="42"
+              height="400"
+              viewBox="0 0 43 629"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute -left-10"
+            >
+              <rect x="20" y="43" width="4" height="1034" fill="white" />
+              <circle
+                cx="21.5"
+                cy="21.5"
+                r="20"
+                fill="#27187E"
+                stroke="#00FFD1"
+                strokeWidth="3"
+              />
+              <circle cx="21.5" cy="21.5" r="13.5" fill="#00FFD1" />
+            </svg>
+            <span className="font-semibold">11-2021:</span> Graduated the
+            Reactjs Web Development at Re:Coded 🎉
+          </li>
+          <li className="max-w-md relative pb-10 mb-10 cursor-pointer shadow-xl rounded-lg hover:bg-darkPrimary transition-all duration-300">
+            <div className="absolute -left-7 bg-white w-5 h-5 rounded-full"></div>
+            <span className="font-semibold">8-2021: </span>Participated in
+            Reactjs Bootcamp at Re:Coded.
+          </li>
+          <li className="max-w-md relative pb-10 mb-10 cursor-pointer shadow-xl rounded-lg hover:bg-darkPrimary transition-all duration-300">
+            <div className="absolute -left-7 bg-white w-5 h-5 rounded-full"></div>
+            <span className="font-semibold">11-2020: </span>Got into Software
+            Engineering college.
+          </li>
+          <li className="max-w-md relative pb-10 mb-10 cursor-pointer shadow-xl rounded-lg hover:bg-darkPrimary transition-all duration-300">
+            <div className="absolute -left-7 bg-white w-5 h-5 rounded-full"></div>
+            <span className="font-semibold">6-2020: </span>Started to learn
+            programming with HTML, CSS and JavaScript.
+          </li>
         </ul>
       </div>
     </div>
