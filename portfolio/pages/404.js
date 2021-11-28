b9b42445d8f46ab99689ebcecb12b9200e652d2f
RawandDev <rawand.200091384037@gmail.com>
2021-11-28 23:39:37 +0300

    feat(error): style error page

--- portfolio/pages/404.js
+++ portfolio/pages/404.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import error from "../public/images/error.svg";
 
 function Custom404() {
   return (
-    <div>
-      <h1>404</h1>
+    <div className="flex flex-col justify-center items-center mt-20 text-white">
+      <h1 className="font-bold text-2xl">Page Not Found</h1>
+      <p className="text-lg">It looks like the page you are looking for was not found</p>
+      <button className="mb-10 bg-white text-black font-bold py-2 px-6 rounded-lg shadow-lg hover:bg-gray-100 hover:text-black focus:outline-none focus:shadow-outline transition-all duration-300 mt-5">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </button>
+      <div className="max-w-lg sm:max-w-2xl">
+        <Image src={error} />
+      </div>
     </div>
   );
 }
