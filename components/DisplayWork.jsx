38de9767642c6bb51a116cd733219b317a8db93a
RawandDev <rawand.200091384037@gmail.com>
2021-11-29 19:19:31 +0300

    refactor(work): refactor work section with adding more projects

--- components/DisplayWork.jsx
+++ components/DisplayWork.jsx
@@ -7,41 +7,58 @@ function DisplayWork() {
     <>
       {workData.map((work, index) => {
         return (
-          <div
-            key={index}
-            className="absolute top-20 flex flex-col lg:flex-row"
-          >
-            <a href={work.demo} target="_blank" rel="noopener noreferrer">
-              <Image
-                src={work.imageURL}
-                height={440}
-                width={600}
-                alt="libvery"
-                className="rounded-md"
-              />
-            </a>
-            <div className="text-white text-center lg:ml-10">
-              <h1 className="text-2xl font-semibold">{work.title}</h1>
-              <p>{work.description}</p>
-              <p>
-                <span className="font-bold">Techs:</span> {work.tech}
-              </p>
-              <div className="text-center mt-4">
-                <a
-                  href={work.demo}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="mr-4"
-                >
-                  <button className="bg-white text-black hover:bg-gray-200 hover:text-black font-medium py-1 px-3 rounded-md">
-                    Demo
-                  </button>
-                </a>
-                <a href={work.github} target="_blank" rel="noopener noreferrer">
-                  <button className="bg-white text-black hover:bg-gray-200 hover:text-black font-medium py-1 px-3 rounded-md">
-                    Github
-                  </button>
-                </a>
+          <div key={work.title}>
+            <p className="font-extrabold text-9xl opacity-30 text-white">
+              {work.no}
+            </p>
+            <div
+              key={index}
+              className={`flex flex-col justify-around ${
+                work.isReversed ? "lg:flex-row-reverse" : "lg:flex-row"
+              } mb-32 -mt-10`}
+            >
+              <a
+                href={work.demo}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="border-2 border-opacity-20 rounded-md"
+              >
+                <Image
+                  src={work.imageURL}
+                  height={440}
+                  width={600}
+                  priority={true}
+                  alt={work.title}
+                  className="rounded-md object-contain"
+                />
+              </a>
+              <div className="text-white text-center mt-3">
+                <h1 className="text-2xl font-semibold">{work.title}</h1>
+                <p>{work.description}</p>
+                <p>
+                  <span className="font-bold">Techs:</span> {work.tech}
+                </p>
+                <div className="text-center mt-4">
+                  <a
+                    href={work.demo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mr-4"
+                  >
+                    <button className="bg-white text-black hover:bg-gray-200 hover:text-black font-medium py-1 px-3 rounded-md">
+                      Demo
+                    </button>
+                  </a>
+                  <a
+                    href={work.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button className="bg-white text-black hover:bg-gray-200 hover:text-black font-medium py-1 px-3 rounded-md">
+                      Github
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
--- components/Work.jsx
+++ components/Work.jsx
@@ -5,7 +5,6 @@ function Work() {
   return (
     <div id="work" className="text-white">
       <div className="relative">
-        <p className="font-extrabold text-9xl opacity-50 text-white">.01</p>
         <DisplayWork />
       </div>
     </div>
