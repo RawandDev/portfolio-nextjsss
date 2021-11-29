8b7806b5ea219a31439a0a0182d99c9b6114b8cc
RawandDev <rawand.200091384037@gmail.com>
2021-11-29 20:13:21 +0300

    feat(google-analytics): add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,29 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div className="font-body mx-4 sm:mx-20">
       <Head>
