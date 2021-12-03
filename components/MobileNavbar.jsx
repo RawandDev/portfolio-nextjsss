f0891b9c25d13a1f4a4e41828af2c7a8de3a6e8f
RawandDev <rawand.200091384037@gmail.com>
2021-12-03 22:52:52 +0300

    feat(navbar): add sticky navbar (if perf issue, come back to these 2 latest commit)

--- components/MobileNavbar.jsx
+++ components/MobileNavbar.jsx
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
-function MobileNavbar({ isOpened }) {
+function MobileNavbar({ isOpened, showActions }) {
   return (
     <>
       {isOpened && (
-        <ul className="md:hidden flex flex-col text-white text-xl">
+        <motion.ul
+          animate={{ opacity: showActions ? 1 : 0 }}
+          className="md:hidden flex flex-col text-white text-2xl mt-32 fixed left-0 pl-20 z-10 -top-9 bg-primary w-full"
+        >
           <Link href="/">
-            <a>Home</a>
+            <a className="mb-2">Home</a>
           </Link>
           <Link href="/work">
-            <a>Work</a>
+            <a className="mb-2">Work</a>
           </Link>
           <Link href="/about">
-            <a>About</a>
+            <a className="mb-2">About</a>
           </Link>
           <Link href="/contact">
-            <a>Contact</a>
+            <a className="mb-2">Contact</a>
           </Link>
-        </ul>
+        </motion.ul>
       )}
     </>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "../public/images/logo.svg";
 import MobileNavbar from "./MobileNavbar";
 import NavbarButtons from "./NavbarButtons";
+import { motion } from "framer-motion";
 
 function Navbar() {
   const [isOpened, setIsOpened] = useState(false);
+  const [lastYPos, setLastYPos] = useState(0);
+  const [showActions, setShowActions] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const yPos = window.scrollY;
+      setShowActions(lastYPos > yPos);
+      setLastYPos(yPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [showActions, lastYPos]);
 
   return (
     <div>
-      <div className="text-white md:flex md:justify-between md:items-center transform transition-all top-0 z-10 flex justify-between relative">
+      <motion.div
+        animate={{ opacity: showActions ? 1 : 0 }}
+        className="text-white md:flex md:justify-between md:items-center transform transition-all top-0 z-10 flex justify-between fixed w-full left-0 bg-primary px-20"
+      >
         <Link href="/">
           <a>
             <Image src={Logo} alt="logo" />
@@ -33,8 +52,13 @@ function Navbar() {
           </ul>
         </div>
         <NavbarButtons isOpened={isOpened} setIsOpened={setIsOpened} />
-      </div>
-      <MobileNavbar isOpened={isOpened} setIsOpened={setIsOpened} />
+      </motion.div>
+      <MobileNavbar
+        isOpened={isOpened}
+        setIsOpened={setIsOpened}
+        lastYPos={lastYPos}
+        showActions={showActions}
+      />
     </div>
   );
 }
