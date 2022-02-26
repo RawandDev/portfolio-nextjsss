e87b8f60fe1a9e57c8f7c5b63e0105f78d324379
RawandDev <rawand.200091384037@gmail.com>
2022-02-27 00:08:35 +0300

    fix(achievement-details): fix while in details clicking on nav

--- components/Navbar/MobileNavbar.jsx
+++ components/Navbar/MobileNavbar.jsx
@@ -1,17 +1,45 @@
 import React from "react";
+import { useRouter } from "next/router";
 import ToggleDarkMode from "./ToggleDarkMode";
+import Link from "next/link";
 
 function MobileNavbar({ isOpened }) {
+  const router = useRouter();
+
   return (
     <>
-      {isOpened && (
+      {isOpened && router.pathname.includes("/achievement") ? (
+        <ul className="md:hidden flex flex-col dark:text-white text-xl">
+          <li className="hover:text-secondary">
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li className="hover:text-secondary">
+            <Link href="/">
+              <a>Work</a>
+            </Link>
+          </li>
+          <li className="hover:text-secondary">
+            <Link href="/">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className="hover:text-secondary">
+            <Link href="/">
+              <a>Contact</a>
+            </Link>
+          </li>
+          <ToggleDarkMode />
+        </ul>
+      ) : isOpened && !router.pathname.includes("/achievement") ? (
         <ul className="md:hidden flex flex-col dark:text-white text-xl">
           <li>
             <a href="#home">Home</a>
           </li>
           <li>
             <a href="#work">Work</a>
-          </li>{" "}
+          </li>
           <li>
             <a href="#about">About</a>
           </li>
@@ -20,7 +48,7 @@ function MobileNavbar({ isOpened }) {
           </li>
           <ToggleDarkMode />
         </ul>
-      )}
+      ) : null}
     </>
   );
 }
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,34 +1,72 @@
 import React, { useState } from "react";
 import MobileNavbar from "./MobileNavbar";
 import NavbarButtons from "../Navbar/NavbarButtons";
-import Image from "next/image";
+import Link from "next/link";
 import ToggleDarkMode from "./ToggleDarkMode";
+import { useRouter } from "next/router";
 
 function Navbar() {
   const [isOpened, setIsOpened] = useState(false);
+
+  const router = useRouter();
 
   return (
     <div>
       <div className="dark:text-white md:flex md:justify-between md:items-center transform transition-all top-0 z-10 flex justify-between relative pt-3">
         <div className="p-3 text-xl">
-          <a href="#home">
-            Ra<span className="text-secondary">wand</span>
-          </a>
+          {router.pathname.includes("/achievement") ? (
+            <Link href="/">
+              <a>
+                Ra<span className="text-secondary">wand</span>
+              </a>
+            </Link>
+          ) : (
+            <a href="#home">
+              Ra<span className="text-secondary">wand</span>
+            </a>
+          )}
         </div>
         <div>
           <ul className="md:flex md:justify-between md:w-96 text-xl hidden items-center">
-            <li className="hover:text-secondary">
-              <a href="#home">Home</a>
-            </li>
-            <li className="hover:text-secondary">
-              <a href="#work">Work</a>
-            </li>
-            <li className="hover:text-secondary">
-              <a href="#about">About</a>
-            </li>
-            <li className="hover:text-secondary">
-              <a href="#contact">Contact</a>
-            </li>
+            {router.pathname.includes("/achievement") ? (
+              <>
+                <li className="hover:text-secondary">
+                  <Link href="/">
+                    <a>Home</a>
+                  </Link>
+                </li>
+                <li className="hover:text-secondary">
+                  <Link href="/">
+                    <a>Work</a>
+                  </Link>
+                </li>
+                <li className="hover:text-secondary">
+                  <Link href="/">
+                    <a>About</a>
+                  </Link>
+                </li>
+                <li className="hover:text-secondary">
+                  <Link href="/">
+                    <a>Contact</a>
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="hover:text-secondary">
+                  <a href="#home">Home</a>
+                </li>
+                <li className="hover:text-secondary">
+                  <a href="#work">Work</a>
+                </li>
+                <li className="hover:text-secondary">
+                  <a href="#about">About</a>
+                </li>
+                <li className="hover:text-secondary">
+                  <a href="#contact">Contact</a>
+                </li>
+              </>
+            )}
             <ToggleDarkMode />
           </ul>
         </div>
