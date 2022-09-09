8bc3ec7470f419bbc13a192ab8e9f8807b3597fd
RawandDev <rawand.200091384037@gmail.com>
2022-09-09 21:06:54 +0300

    remove: remove google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import Head from "next/head";
-import Script from "next/script";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer";
 import NextNProgress from "nextjs-progressbar";
@@ -9,23 +8,6 @@ import NextNProgress from "nextjs-progressbar";
 function MyApp({ Component, pageProps }) {
   return (
     <div className="font-body px-4 sm:px-20 dark:bg-primary bg-[#FFF8F3]">
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`
-        if (window.location.hostname !== 'localhost') {
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-              });
-            }
-                `}
-      </Script>
       <Head>
         <title>RawaDev</title>
       </Head>
